923080d4e9846647ff3ec572108a52823b76df8e
Devansh Agarwal <devanshagar783@gmail.com>
2023-07-17 00:31:21 +0530

    3d mocdels refined

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { slideAnimation, fadeAnimation } from "../config/motion";
+import { EditorTabs, DecalTypes, FilterTabs } from "../config/constants";
+import config from "../config/config";
+import {
+    AIPicker,
+    ColorPicker,
+    CustomButton,
+    FilePicker,
+    Tab,
+} from "../components";
 
 const Customizer = () => {
-  return (
-    <div>
-      Customizer
-    </div>
-  )
-}
+    const snap = useSnapshot(state);
 
-export default Customizer
+    const [file, setFile] = useState("");
+    const [promt, setPromt] = useState("");
+    const [generatingImage, setGeneratingImage] = useState(false);
+    const [activeEditorTab, setActiveEditorTab] = useState("");
+    const [activeFilterTab, setActiveFilterTab] = useState({
+        logoShirt: true,
+        stylishShirt: false,
+    });
+
+    const generateTabContent = () => {
+        switch (activeEditorTab) {
+            case "colorpicker":
+                return <ColorPicker />;
+            case "filepicker":
+                return <FilePicker />;
+            case "aipicker":
+                return <AIPicker />;
+            default:
+                return null;
+        }
+    };
+
+    return (
+        <AnimatePresence>
+            {!snap.intro && (
+                <>
+                    <motion.div
+                        key="custom"
+                        className="absolute top-0 left-0 z-10"
+                        {...slideAnimation("left")}
+                    >
+                        <div className="flex items-center min-h-screen">
+                            <div className="editortabs-container tabs">
+                                {EditorTabs.map((tab) => (
+                                    <Tab
+                                        key={tab.name || ""}
+                                        tab={tab}
+                                        handleClick={() => {
+                                            setActiveEditorTab(tab.name);
+                                        }}
+                                    ></Tab>
+                                ))}
+                                {generateTabContent()}
+                            </div>
+                        </div>
+                    </motion.div>
+
+                    <motion.div
+                        className="absolute z-10 top-5 right-5"
+                        {...fadeAnimation}
+                    >
+                        <CustomButton
+                            type={"filled"}
+                            title={"Go Back"}
+                            handleClick={() => (state.intro = true)}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
+                    </motion.div>
+
+                    <motion.div
+                        className="filtertabs-container"
+                        {...slideAnimation("up")}
+                    >
+                        {FilterTabs.map((tab) => (
+                            <Tab
+                                key={tab.name}
+                                tab={tab}
+                                handleClick={() => {}}
+                                isFilterTab
+                                isActiveTab=""
+                            />
+                        ))}
+                    </motion.div>
+                </>
+            )}
+        </AnimatePresence>
+    );
+};
+
+export default Customizer;
